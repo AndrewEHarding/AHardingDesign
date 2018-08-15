182f85038f1b2919432857cfcedda3e6aa21f37c
Andrew Harding <dreweharding@gmail.com>
2018-08-15 02:11:30 -0400

    parallax working on all images

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
 
+    $(window).on("scroll", function(){
+
+        let offset = window.pageYOffset;
+        let paraOffset = offset * 0.7;
+
+        $(".scroll1").css("backgroundPositionY", paraOffset + "px");
+        $(".scroll2").css("backgroundPositionY", (paraOffset + 600) + "px");
+        $(".scroll3").css("backgroundPositionY", paraOffset + "px");
+    });
+
 
 });//End of document.ready
