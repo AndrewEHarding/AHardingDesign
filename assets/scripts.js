f0d4e2e7cd73b0778fa25c6ee1c7379755b84e02
Andrew Harding <dreweharding@gmail.com>
2018-08-16 02:01:03 -0400

    spent too much time tweeking portfolio and grids

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,19 +8,24 @@ $(document).ready(function () {
         }, 500);
     });
 
+    // SCROLL FUNCTIONS
     $(window).on("scroll", function () {
-
         let offset = window.pageYOffset;
         let paraOffset = offset * 0.7;
-
         $(".scroll1").css("backgroundPositionY", paraOffset + "px");
         $(".scroll2").css("backgroundPositionY", (paraOffset + 600) + "px");
         $(".scroll3").css("backgroundPositionY", paraOffset + "px");
-
         var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
         var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
         var scrolled = (winScroll / height) * 100;
         document.getElementById("myBar").style.width = scrolled + "%";
+    });
+
+    // SUBMIT FUNCTIONS
+    $("#submit").on("click", function(event){
+        event.preventDefault();
+        M.toast({html: 'Empty Fields'});
+
     });
 
 
